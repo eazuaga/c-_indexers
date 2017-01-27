ce76fb9656d511260bc455fcdae52ddfcfcc9bf7
eduardo azuaga <eduardo.azuaga@autologica.com>
2017-01-27 16:20:22 -0300

    come gato

---
+++ Indexadores/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            colecctions week = new colecctions();
+            System.Console.WriteLine(week["Sun"]);
+            Console.ReadKey();
+
+        }
+    }
+
+    internal class colecctions
+    {
+        private string[] dias = {"Sun", "Mon", "Tues"};
+        private int _type;
+
+        private int GetDay(string testDay)
+        {
+            for (int i = 0; i < dias.Length; i++)
+            {
+                if (dias[i] == testDay)
+                {
+                    return i;
+                } 
+            }
+            throw new System.ArgumentOutOfRangeException(testDay, "el dia debe estar de lunes a domingos");
+        }
+        public int this[string dia]
+        {
+           get
+            {
+                return (GetDay(dia));
+            }
+        }
+
+    }
+}
